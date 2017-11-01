d4032628db272b43d68bf236b027614395f15070
Urban Lindström <urban.lindstrom@boosted.se>
2017-11-01 15:15:06 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>naltabalubas</title>
+</head>
+<body>
+	<form action="receive.php" method="post">
+		<fieldset>
+			<legend>kontaktuppgifter</legend>
+			<label for="nickname">Smeknamn:</label>
+			<input type="text" name="nickname"><br>
+			<label for="phone">Telefon:</label>
+			<input type="tel" name="phone">
+		</fieldset>
+		<input type="submit" name="Submit" value="Skicka">
+	</form>
+
+</body>
+</html>
